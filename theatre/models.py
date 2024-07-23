eb039cfd22fc035059d3297b6f7679518002506a
pavlo <frontbastard@gmail.com>
2024-07-23 16:03:23 +0200

    Create models for theatre and user apps
    - Created migrations for `theatre` and `user` apps
    - Modified `admin.py` for `theatre` and `user` apps
    - Added `AUTH_USER_MODEL` to `settings.py`
    - Made migrations

--- theatre/models.py
+++ theatre/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class TheatreHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    def __str__(self):
+        return f"Hall #{self.id}: {self.name}"
