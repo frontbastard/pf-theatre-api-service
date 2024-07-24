98e3ba9005cd2053b6d98a3a7193465a10c1defc
pavlo <frontbastard@gmail.com>
2024-07-24 16:48:13 +0200

    Add Tickets TabularInline for Reservation in admin panel

--- theatre/admin.py
+++ theatre/admin.py
@@ -1,5 +1,29 @@
 from django.contrib import admin
 
-from theatre.models import TheatreHall
+from theatre.models import (
+    TheatreHall,
+    Ticket,
+    Performance,
+    Reservation,
+    Genre,
+    Actor,
+    Play
+)
+
+
+class TicketInline(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    inlines = (TicketInline,)
+
 
 admin.site.register(TheatreHall)
+admin.site.register(Ticket)
+admin.site.register(Performance)
+admin.site.register(Genre)
+admin.site.register(Actor)
+admin.site.register(Play)
