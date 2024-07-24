7de75d44c3a95d978deb89e43e71da8f8a244ab4
pavlo <frontbastard@gmail.com>
2024-07-24 16:09:15 +0200

    Add new models
    - Added models Genre, Actor, Play, Performance, Reservation, Ticket
    - For `Performance` model overridden methods `clean` and `save` for validating that show_time property not in the past
    - Created validator validate_ticket for Ticket model and used this in overridden `clean` (also used `save`)
    - In Ticket model added `UniqueConstraint` for row, seat and performance
    - Made migrations

--- theatre/models.py
+++ theatre/models.py
@@ -1,10 +1,131 @@
+from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.db.models import UniqueConstraint
+from django.utils import timezone
 
 
 class TheatreHall(models.Model):
     name = models.CharField(max_length=255)
-    rows = models.IntegerField()
-    seats_in_row = models.IntegerField()
+    rows = models.PositiveIntegerField()
+    seats_in_row = models.PositiveIntegerField()
 
     def __str__(self):
         return f"Hall #{self.id}: {self.name}"
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    @property
+    def full_name(self):
+        return self.__str__()
+
+
+class Play(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    actors = models.ManyToManyField(Actor, blank=True, related_name="plays")
+    genres = models.ManyToManyField(Genre, blank=True, related_name="plays")
+
+    def __str__(self):
+        return self.title
+
+
+class Performance(models.Model):
+    play = models.ForeignKey(
+        Play,
+        on_delete=models.CASCADE,
+        related_name="performances"
+    )
+    theatre_hall = models.ForeignKey(TheatreHall, on_delete=models.CASCADE)
+    show_time = models.DateTimeField(db_index=True)
+
+    def __str__(self):
+        return f"{self.play.title} {str(self.show_time)}"
+
+    def clean(self):
+        if self.show_time < timezone.now():
+            raise ValidationError("Show time cannot be in the past.")
+
+    def save( self, *args, **kwargs):
+        self.full_clean()
+        return super().save(*args, **kwargs)
+
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"Reservation #{self.id} by {self.user}"
+
+
+class Ticket(models.Model):
+    row = models.PositiveIntegerField()
+    seat = models.PositiveIntegerField()
+    performance = models.ForeignKey(
+        Performance,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+    reservation = models.ForeignKey(
+        Reservation,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+
+    @staticmethod
+    def validate_ticket(row, seat, theatre_hall, error_to_raise):
+        for ticket_attr_value, ticket_attr_name, theatre_hall_attr_name in [
+            (row, "row", "rows"),
+            (seat, "seat", "seats_in_row")
+        ]:
+            count_attrs = getattr(theatre_hall, theatre_hall_attr_name)
+
+            if not (1 <= ticket_attr_value <= count_attrs):
+                raise error_to_raise(
+                    {
+                        ticket_attr_name: f"{ticket_attr_name} "
+                        f"number must be in available range: "
+                        f"(1, {theatre_hall_attr_name}): "
+                        f"(1, {count_attrs})"
+                    }
+                )
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                fields=["row", "seat", "performance"],
+                name="unique_ticket_row_seat_performance"
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.performance} - (row: {self.row}, seat: {self.seat})"
+
+    def clean(self):
+        Ticket.validate_ticket(
+            self.row,
+            self.seat,
+            self.performance.theatre_hall,
+            ValidationError,
+        )
+
+    def save( self, *args, **kwargs):
+        self.full_clean()
+        return super().save(*args, **kwargs)
