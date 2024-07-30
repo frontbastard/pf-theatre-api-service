d307c01a726803ba7d30c0a46aae04e5e9fe3734
pavlo <frontbastard@gmail.com>
2024-07-30 15:02:23 +0200

    Add throttle
    ## settings.py
    
    - Added DEFAULT_THROTTLE_CLASSES
    - Added DEFAULT_THROTTLE_RATES
    
    The setting helps to limit access to the app for anonymous users to 10 times per day and for logged-in users to 30 times per day.

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -140,6 +140,11 @@ REST_FRAMEWORK = {
         "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 5,
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "10/day", "user": "30/day"},
 }
 
 SPECTACULAR_SETTINGS = {
