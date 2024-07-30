0ec986933878ddce261521ebd909e541279c60aa
pavlo <frontbastard@gmail.com>
2024-07-30 15:40:10 +0200

    Add JWT token instead of common authentication
    - Set DEFAULT_AUTHENTICATION_CLASSES in settings.py
    - Removed authentication_classes from each view
    - Added routes for obtain, refresh and verify token in user/urls.py
    - Customized SIMPLE_JWT in settings.py

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -161,4 +161,10 @@ SPECTACULAR_SETTINGS = {
         "defaultModelsExpandDepth": 2,
         "defaultModelExpandDepth": 2,
     },
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=15),
+    "ROTATE_REFRESH_TOKENS": False,
 }
