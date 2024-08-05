bb080e2add7b3799b6380eb09c06b8b8d8911ac6
pavlo <frontbastard@gmail.com>
2024-08-05 21:54:37 +0200

    Refactor create_superuser command
    - Added check whether SUPERUSER_EMAIL exists
    - Added check whether SUPERUSER_PASSWORD exists
    - If it's not raise an error UndefinedValueError

--- theatre/management/commands/create_superuser.py
+++ theatre/management/commands/create_superuser.py
@@ -1,6 +1,8 @@
-from decouple import config
-from django.core.management.base import BaseCommand
+import sys
+
+from decouple import config, UndefinedValueError
 from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
 from django.db.utils import IntegrityError
 
 
@@ -8,12 +10,28 @@ class Command(BaseCommand):
     help = "Create a superuser if none exist"  # noqa: VNE003
 
     def handle(self, *args, **options):
+        try:
+            email = config("SUPERUSER_EMAIL")
+            password = config("SUPERUSER_PASSWORD")
+
+            if not email or not password:
+                raise UndefinedValueError
+        except UndefinedValueError:
+            self.stdout.write(
+                self.style.ERROR(
+                    "ERROR: SUPERUSER_EMAIL or "
+                    "SUPERUSER_PASSWORD not set in .env"
+                )
+            )
+            sys.exit(1)
+
         user = get_user_model()
         if not user.objects.filter(is_superuser=True).exists():
+
             try:
                 user.objects.create_superuser(
-                    email=config("SUPERUSER_EMAIL"),
-                    password=config("SUPERUSER_PASSWORD"),
+                    email=email,
+                    password=password,
                 )
                 self.stdout.write(self.style.SUCCESS("Superuser created."))
             except IntegrityError:
